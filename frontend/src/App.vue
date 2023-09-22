7a70cdc8448409bf1b146d23c4a5c8e2324e864b
marko.medic <marko.medic@aws.talpanetwork.com>
2023-09-22 18:22:35 +0200

    Init frontend app

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>Vue app...</h3>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "App",
+  setup() {
+    axios
+      .get("http://localhost:4000/shop")
+      .then((resp) => {
+        console.log("@resp!", resp?.data);
+      })
+      .catch((e) => console.log(e));
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
